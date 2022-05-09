a99ee95e9ec4bb949ca1db7131aee3aa45d3ab47
Ismaelforest <ismaelforest@gmail.com>
2022-05-08 21:05:31 -0400

    sdd

--- src/main/kotlin/com/example/routes/rabbitRoute.kt
+++ src/main/kotlin/com/example/routes/rabbitRoute.kt
@@ -3,8 +3,10 @@ package com.example.routes
 import com.example.data.Rabbit
 import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import io.netty.handler.codec.http.HttpMethod.POST
 
 private const val BASE_URL = "https://api25806.herokuapp.com/"
 
@@ -19,6 +21,14 @@ fun Route.randomRabbit() {
     }
     get("/") {
         call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
+    }
+    post("/post") {
+        val parameters = call.receiveParameters()
+
+        val paramVal1 = parameters["param1"]
+        val paramVal2 = parameters["param2"]
+
+        call.respondText("This is a test POST request with parameter values $paramVal1 and $paramVal2")
     }
 
 }
