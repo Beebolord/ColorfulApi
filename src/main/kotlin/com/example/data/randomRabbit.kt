6e88df570d344927e97b95fe30ec6ea70df33ecd
Ismaelforest <ismaelforest@gmail.com>
2022-06-14 19:01:43 -0400

    My first commit

---
+++ src/main/kotlin/com/example/data/randomRabbit.kt
@@ -0,0 +1,41 @@
+import com.example.data.HexoCode
+import com.example.data.Rabbit
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+
+
+private const val BASE_URL = "https://api25806.herokuapp.com"
+
+private val rabbits = listOf(
+    Rabbit("Carl","Brown rabbi","$BASE_URL/rabbits/rabbit1.png")
+)
+
+private val hexes = listOf (
+    HexoCode("0xff3399","First"),
+    HexoCode("0xff33FF","Second")
+)
+fun Route.randomRabbit() {
+    get("/randomRabbit") {
+        call.respond(
+            HttpStatusCode.OK,
+        )
+    }
+    get("/") {
+        call.respondText("HELLO WORLD I changed!...once again", contentType = ContentType.Text.Plain)
+    }
+
+    get("/test") {
+        call.respondText("Message received",contentType = ContentType.Text.Plain)
+    }
+    get("/firstBall") {
+        call.respond(HttpStatusCode.OK,hexes)
+    }
+    post("/postting") {
+        call.respondText("yoooooooooo")
+    }
+
+
+}
